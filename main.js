07527b8e0e410dc8a2ed21519167ce46d530568f
Rae Liu <raeliu@brightroll.com>
2015-06-08 15:21:04 -0700

    add packge.json

---
+++ main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  Scroller: require('./src/Scroller'),
+  Animate: require('./src/Animate'),
+};
--- src/Animate.js
+++ src/Animate.js
@@ -22,7 +22,7 @@
  * rendering. This eases a lot of cases where it might be pretty complex to break down a state
  * based on the pure time difference.
  */
-(function(global) {
+var Animate = function (global) {
 	var time = Date.now || function() {
 		return +new Date();
 	};
@@ -235,5 +235,6 @@
 			return id;
 		}
 	};
-})(this);
+};
 
+module.exports = Animate;
